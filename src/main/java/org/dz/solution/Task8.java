f73c658f8c3831dff4be852390acc913a47b2441
anassvoronn <UQzbwQCt7R3uMfz>
2022-09-03 14:08:34 +0300

    Task8

---
+++ src/main/java/org/dz/solution/Task8.java
@@ -0,0 +1,48 @@
+package org.dz.solution;
+
+import java.util.Random;
+
+/*
+Создайте массив из 12 случайных целых чисел
+из отрезка [-15;15]. Определите какой элемент
+является в этом массиве максимальным и сообщите
+индекс его последнего вхождения в массив.
+[-13, -7, 5, 0, 5, -13]
+ */
+public class Task8 {
+    public void printArray() {
+        int[] array = new int[12];
+
+        fillArrayWithRandomNumbers(array);
+
+        printArray(array);
+        System.out.println();
+        determineTheMaxElementInAnArray(array);
+    }
+
+    private void fillArrayWithRandomNumbers(final int[] array) {
+        Random generator = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = generator.nextInt(31) - 15;
+        }
+    }
+
+    private void printArray(final int[] array) {
+        for (int item : array) {
+            System.out.print(item + " ");
+        }
+    }
+
+    private void determineTheMaxElementInAnArray(final int[] array) {
+        int maxNumber = 0;
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] >= maxNumber) {
+                maxNumber = array[i];
+                index = i;
+            }
+        }
+        System.out.println(array[index] + " Max Element");
+        System.out.println(index + " Index");
+    }
+}
---
+++ src/test/java/org/dz/solution/Task8Test.java
@@ -0,0 +1,14 @@
+package org.dz.solution;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Task8Test {
+
+    @Test
+    public void printArray() {
+        Task8 task8 = new Task8();
+        task8.printArray();
+    }
+}
