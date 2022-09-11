98c84a7b57ded4e042b8abb692f85fbebe2d21e7
anassvoronn <UQzbwQCt7R3uMfz>
2022-09-11 17:20:30 +0300

    Task12

---
+++ src/test/java/org/dz/solution/Task12Test.java
@@ -0,0 +1,14 @@
+package org.dz.solution;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Task12Test {
+
+    @Test
+    public void printResult() {
+        Task12 task12 = new Task12();
+        task12.printResult();
+    }
+}
