9fafb9d26e3f718900d1ec5e7de44ad5ad5abb69
anassvoronn <UQzbwQCt7R3uMfz>
2022-09-11 16:26:17 +0300

    Task11

---
+++ src/test/java/org/dz/solution/Task11Test.java
@@ -0,0 +1,14 @@
+package org.dz.solution;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Task11Test {
+
+    @Test
+    public void printResult() {
+        Task11 task11 = new Task11();
+        task11.printResult();
+    }
+}
