51e53b9af8741c5893280f275f78041cbca64f5a
anassvoronn <UQzbwQCt7R3uMfz>
2022-10-10 21:46:43 +0300

    Task16

---
+++ src/test/java/org/dz/solution/Task16Test.java
@@ -0,0 +1,14 @@
+package org.dz.solution;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Task16Test {
+
+    @Test
+    public void printResult() {
+        Task16 task16 = new Task16();
+        task16.printResult();
+    }
+}
