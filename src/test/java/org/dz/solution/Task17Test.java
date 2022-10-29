4a48d2533234b9e768e3072f7ea47cac7d265df5
Anassvoronn <96254900+anassvoronn@users.noreply.github.com>
2022-10-29 18:11:04 +0300

    Task17 (#15)
    Co-authored-by: anassvoronn <UQzbwQCt7R3uMfz>

---
+++ src/test/java/org/dz/solution/Task17Test.java
@@ -0,0 +1,14 @@
+package org.dz.solution;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Task17Test {
+
+    @Test
+    public void printResult() {
+        Task17 task17 = new Task17();
+        task17.printResult();
+    }
+}
