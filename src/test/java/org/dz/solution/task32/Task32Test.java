ac5c8c2d8a6f26ca64e81c9db8948659e1cffad2
anassvoronn <UQzbwQCt7R3uMfz>
2023-06-24 22:39:59 +0300

    Task32

--- src/test/java/org/dz/solution/task32/Task32Test.java
+++ src/test/java/org/dz/solution/task32/Task32Test.java
@@ -7,60 +7,39 @@ import java.io.*;
 
 public class Task32Test {
 
-    public static final String WORD_DELIMITER = " ";
-
     @Test
     public void test() throws IOException {
         int[] array = {1, 3, 4, 6, 9, 10, 12, 13, 14, 16, 19, 20};
         String filePath = "src/main/resources/org/dz/solution/task32/InputFile";
-
         String filePathTwo = "src/main/resources/org/dz/solution/task32/OutputFile";
+        PrintWriter pw = new PrintWriter(filePathTwo);
         int min = array[0];
         int max = array[array.length - 1];
-
-        for (int i = min; i <= max; i++) {
-            boolean foundNumber = false;
-            for (int j = 0; j < array.length; j++) {
-                if (array[j] == i) {
-                    foundNumber = true;
-                    break;
-                }
-            }
-            if (!foundNumber) {
-                System.out.print(i + " ");
-            }
-        }
-    }
-
-    private String[][] createMatrixFromFile(String filePath) {
-        int numberOfLines = 0;
-        int numberOfColumns = 0;
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                numberOfLines++;
-                String[] words = line.split(WORD_DELIMITER);
-                if (words.length > numberOfColumns) {
-                    numberOfColumns = words.length;
+            String line = br.readLine();
+            String[] elements = line.split(" ");
+            array = new int[elements.length];
+            for (int i = 0; i < elements.length; i++) {
+                array[i] = Integer.parseInt(elements[i]);
+            }
+            br.close();
+            for (int i = min; i <= max; i++) {
+                boolean foundNumber = false;
+                for (int j = 0; j < array.length; j++) {
+                    if (array[j] == i) {
+                        foundNumber = true;
+                        break;
+                    }
+                }
+                if (!foundNumber) {
+                    System.out.print(i + " ");
+                    pw.print(i + " ");
                 }
             }
+            pw.close();
         } catch (IOException e) {
             throw new RuntimeException("Matrix creation failed", e);
         }
-        return new String[numberOfLines][numberOfColumns];
     }
 
-    private void fillInTheMatrix(String filePath, String[][] matrix) {
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            for (int i = 0; i < matrix.length; i++) {
-                String line = br.readLine();
-                String[] words = line.split(WORD_DELIMITER);
-                for (int j = 0; j < words.length; j++) {
-                    matrix[i][j] = words[j];
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Filling in the matrix failed", e);
-        }
-    }
 }
