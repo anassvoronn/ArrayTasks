4065b3efc43b75b45dcfb8cdcc952722a49cf384
anassvoronn <UQzbwQCt7R3uMfz>
2023-06-26 15:55:52 +0300

    Task32

--- src/test/java/org/dz/solution/task32/Task32Test.java
+++ src/test/java/org/dz/solution/task32/Task32Test.java
@@ -1,20 +1,22 @@
 package org.dz.solution.task32;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 
 public class Task32Test {
 
     @Test
     public void test() throws IOException {
-        int[] array = {1, 3, 4, 6, 9, 10, 12, 13, 14, 16, 19, 20};
+        int[] array;
         String filePath = "src/main/resources/org/dz/solution/task32/InputFile";
-        String filePathTwo = "src/main/resources/org/dz/solution/task32/OutputFile";
-        PrintWriter pw = new PrintWriter(filePathTwo);
-        int min = array[0];
-        int max = array[array.length - 1];
+        String filePath2 = "src/main/resources/org/dz/solution/task32/OutputFile";
+        Files.delete(Path.of("src/main/resources/org/dz/solution/task32/OutputFile"));
+
+        PrintWriter pw = new PrintWriter(filePath2);
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line = br.readLine();
             String[] elements = line.split(" ");
@@ -23,23 +25,33 @@ public class Task32Test {
                 array[i] = Integer.parseInt(elements[i]);
             }
             br.close();
-            for (int i = min; i <= max; i++) {
-                boolean foundNumber = false;
-                for (int j = 0; j < array.length; j++) {
-                    if (array[j] == i) {
-                        foundNumber = true;
-                        break;
-                    }
-                }
-                if (!foundNumber) {
-                    System.out.print(i + " ");
-                    pw.print(i + " ");
+            pw.print(Arrays.toString(findTheMissingNumbers(array)));
+
+        } catch (IOException e) {
+            throw new RuntimeException("Array creation failed", e);
+        }
+    }
+
+    private int[] findTheMissingNumbers(int[] array){
+        int min = array[0];
+        int max = array[array.length - 1];
+        int[] newArray = new int[max - min + 1];
+        int index = 0;
+        for (int i = min; i <= max; i++) {
+            boolean foundNumber = false;
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] == i) {
+                    foundNumber = true;
+                    break;
                 }
             }
-            pw.close();
-        } catch (IOException e) {
-            throw new RuntimeException("Matrix creation failed", e);
+            if (!foundNumber) {
+                newArray[index] = i;
+                index++;
+                System.out.print(i + " ");
+            }
         }
+        return newArray;
     }
 
 }
