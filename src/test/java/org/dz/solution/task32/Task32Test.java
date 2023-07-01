a7ce346d828e6d3ced0248a831e7aebe3eaa699b
anassvoronn <UQzbwQCt7R3uMfz>
2023-07-01 18:59:22 +0300

    Task32

--- src/test/java/org/dz/solution/task32/Task32Test.java
+++ src/test/java/org/dz/solution/task32/Task32Test.java
@@ -5,52 +5,76 @@ import org.junit.Test;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
 
 public class Task32Test {
-    int[] array;
+
+    private static final String WORD_DELIMITER = " ";
 
     @Test
     public void test() throws IOException {
         String sourceDataFile = "src/main/resources/org/dz/solution/task32/InputFile";
         String resultFile = "src/main/resources/org/dz/solution/task32/OutputFile";
-        Files.delete(Path.of("src/main/resources/org/dz/solution/task32/OutputFile"));
+        Files.deleteIfExists(Path.of("src/main/resources/org/dz/solution/task32/OutputFile"));
 
-        PrintWriter pw = new PrintWriter(resultFile);
+        findTheNumberOfNumbersInAFile(sourceDataFile);
         readingAFile(sourceDataFile);
-        pw.print(Arrays.toString(findTheMissingNumbers(array)));
-        pw.close();
+        findTheMissingNumbers(resultFile);
     }
 
-    private int[] findTheMissingNumbers(int[] array) {
+    private String[] findTheNumberOfNumbersInAFile(String sourceDataFile) {
+        int count = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(sourceDataFile))) {
+            String line = br.readLine();
+            String[] words = line.split(WORD_DELIMITER);
+            for (int i = 0; i < words.length; i++) {
+                if (words.length > count){
+                    count++;
+                }
+            }
+            System.out.print(count);
+        } catch (IOException e) {
+            throw new RuntimeException("Array creation failed", e);
+        }
+        return new String[count];
+    }
+
+    private int[] findTheMissingNumbers(String resultFile) {
+        int[] array = new int[10];
         int min = array[0];
         int max = array[array.length - 1];
         int[] newArray = new int[array.length];
         int index = 0;
-        for (int i = min; i <= max; i++) {
-            boolean foundNumber = false;
-            for (int j = 0; j < array.length; j++) {
-                if (array[j] == i) {
-                    foundNumber = true;
-                    break;
+        try (PrintWriter pw = new PrintWriter(resultFile)) {
+            for (int i = min; i <= max; i++) {
+                boolean foundNumber = false;
+                for (int j = 0; j < array.length; j++) {
+                    if (array[j] == i) {
+                        foundNumber = true;
+                        break;
+                    }
+                }
+                if (!foundNumber) {
+                    newArray[index] = i;
+                    index++;
+                    System.out.print(i + " ");
+                    pw.print(i);
                 }
             }
-            if (!foundNumber) {
-                newArray[index] = i;
-                index++;
-                System.out.print(i + " ");
-            }
+        } catch (IOException e) {
+            throw new RuntimeException("Array creation failed", e);
         }
         return newArray;
     }
 
-    private void readingAFile(String filePath) {
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+    private void readingAFile(String sourceDataFile) {
+        findTheNumberOfNumbersInAFile(sourceDataFile);
+        String[] array = new String[13]; //почемуто становится серым когда пишу число любое в скобки
+        try (BufferedReader br = new BufferedReader(new FileReader(sourceDataFile))) {
             String line = br.readLine();
             String[] elements = line.split(" ");
-            array = new int[elements.length];
+            array = new String[elements.length];
             for (int i = 0; i < elements.length; i++) {
-                array[i] = Integer.parseInt(elements[i]);
+                array[i] = String.valueOf(Integer.parseInt(elements[i]));
             }
         } catch (IOException e) {
             throw new RuntimeException("Array creation failed", e);
