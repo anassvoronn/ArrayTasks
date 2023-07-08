a472f60ba8e6acf252010cec04e74d11a44ff1e8
anassvoronn <UQzbwQCt7R3uMfz>
2023-07-08 16:02:35 +0300

    Task32

--- src/test/java/org/dz/solution/task32/Task32Test.java
+++ src/test/java/org/dz/solution/task32/Task32Test.java
@@ -12,13 +12,14 @@ public class Task32Test {
 
     @Test
     public void test() throws IOException {
+        int[] array = {1, 3, 4, 6, 9, 10, 12, 13, 14, 16, 19, 20};
         String sourceDataFile = "src/main/resources/org/dz/solution/task32/InputFile";
         String resultFile = "src/main/resources/org/dz/solution/task32/OutputFile";
         Files.deleteIfExists(Path.of("src/main/resources/org/dz/solution/task32/OutputFile"));
 
         findTheNumberOfNumbersInAFile(sourceDataFile);
         readingAFile(sourceDataFile);
-        findTheMissingNumbers(resultFile);
+        findTheMissingNumbers(array, resultFile);
     }
 
     private String[] findTheNumberOfNumbersInAFile(String sourceDataFile) {
@@ -27,19 +28,18 @@ public class Task32Test {
             String line = br.readLine();
             String[] words = line.split(WORD_DELIMITER);
             for (int i = 0; i < words.length; i++) {
-                if (words.length > count){
+                if (words.length > count) {
                     count++;
                 }
             }
-            System.out.print(count);
+            System.out.println(count);
         } catch (IOException e) {
             throw new RuntimeException("Array creation failed", e);
         }
         return new String[count];
     }
 
-    private int[] findTheMissingNumbers(String resultFile) {
-        int[] array = new int[10];
+    private int[] findTheMissingNumbers(int[] array, String resultFile) {
         int min = array[0];
         int max = array[array.length - 1];
         int[] newArray = new int[array.length];
@@ -68,13 +68,12 @@ public class Task32Test {
 
     private void readingAFile(String sourceDataFile) {
         findTheNumberOfNumbersInAFile(sourceDataFile);
-        String[] array = new String[13]; //почемуто становится серым когда пишу число любое в скобки
         try (BufferedReader br = new BufferedReader(new FileReader(sourceDataFile))) {
             String line = br.readLine();
             String[] elements = line.split(" ");
-            array = new String[elements.length];
+            int[] array = new int[elements.length];
             for (int i = 0; i < elements.length; i++) {
-                array[i] = String.valueOf(Integer.parseInt(elements[i]));
+                array[i] = Integer.parseInt(elements[i]);
             }
         } catch (IOException e) {
             throw new RuntimeException("Array creation failed", e);
