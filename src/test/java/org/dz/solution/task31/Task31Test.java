e10eef3f823d5cc57ddd139c34e4121d03107378
Anassvoronn <96254900+anassvoronn@users.noreply.github.com>
2023-07-29 18:03:21 +0300

    Task31 (#27)
    * Task31
    
    * Task31
    
    * Task31
    
    * Task31
    
    * Task31
    
    * Task32
    
    * Task32
    
    * Task32
    
    * Task32
    
    * Task32
    
    * Task32
    
    * Task32
    
    * Task32
    
    * Task32
    
    * Task32
    
    * Task32
    
    * Task32 task31
    
    * Task32
    
    * Task32
    
    * Task32
    
    * Task32
    
    * Task32: added assertEquals
    
    * Task32
    
    * Task32: added extra cases
    
    * Task32
    
    * Task32: added extra cases
    
    * Task32
    
    * Task32
    
    * Task32
    
    * Task32
    
    * Task31 added more cases
    
    * Task32
    
    * Task32
    
    * Task32
    
    ---------
    
    Co-authored-by: anassvoronn <UQzbwQCt7R3uMfz>
    Co-authored-by: alexander.budnikov <alexander.budnikov@algosec.com>

---
+++ src/test/java/org/dz/solution/task31/Task31Test.java
@@ -0,0 +1,80 @@
+package org.dz.solution.task31;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Task31Test {
+
+    public static final String WORD_DELIMITER = " ";
+
+    @Test
+    public void writeReadStringMatrixFile() throws IOException {
+        String[][] expectedMatrix = {
+                {"7", "Ок", "Коля", "13", "утка", "45"},
+                {"Phone", "2,5", "string", "@", null, null},
+                {"собака", null, null, null, null, null},
+                {"%", "81", "Задание", "cat", "слово", null}
+        };
+        String filePath = "src/main/resources/org/dz/solution/task31/StringMatrix";
+        Files.delete(Path.of("src/main/resources/org/dz/solution/task31/StringMatrix"));
+
+        String filePathTwo = "src/main/resources/org/dz/solution/task31/StringMatrix";
+
+        printMatrix(expectedMatrix, filePathTwo);
+        String[][] actualMatrix = createMatrixFromFile(filePath);
+        fillInTheMatrix(filePath, actualMatrix);
+        Assert.assertEquals(expectedMatrix, actualMatrix);
+    }
+
+    private void printMatrix(String[][] expectedMatrix, String filePathTwo) {
+        try (PrintWriter pw = new PrintWriter(filePathTwo)) {
+            for (String[] matrix : expectedMatrix) {
+                for (String s : matrix) {
+                    if (s == null) {
+                        break;
+                    }
+                    pw.print(s + " ");
+                }
+                pw.println();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("");
+        }
+    }
+
+    private String[][] createMatrixFromFile(String filePath) {
+        int numberOfLines = 0;
+        int numberOfColumns = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                numberOfLines++;
+                String[] words = line.split(WORD_DELIMITER);
+                if (words.length > numberOfColumns) {
+                    numberOfColumns = words.length;
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Matrix creation failed", e);
+        }
+        return new String[numberOfLines][numberOfColumns];
+    }
+
+    private void fillInTheMatrix(String filePath, String[][] matrix) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            for (int i = 0; i < matrix.length; i++) {
+                String line = br.readLine();
+                String[] words = line.split(WORD_DELIMITER);
+                for (int j = 0; j < words.length; j++) {
+                    matrix[i][j] = words[j];
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Filling in the matrix failed", e);
+        }
+    }
+}
---
+++ src/test/java/org/dz/solution/task32/Task32Test.java
@@ -0,0 +1,209 @@
+package org.dz.solution.task32;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.Assert.assertEquals;
+
+public class Task32Test {
+
+    private static final String SOURCE_DATA_FILE = "src/main/resources/org/dz/solution/task32/InputFile";
+    private static final String RESULT_FILE = "src/main/resources/org/dz/solution/task32/OutputFile";
+    private static final String[] BAD_SYMBOLS = new String[]{",", "-", "\\.", ";"};
+    public static final String WORD_DELIMITER = " ";
+
+    @Before
+    public void setUp() throws Exception {
+        Files.deleteIfExists(Path.of(SOURCE_DATA_FILE));
+        Files.deleteIfExists(Path.of(RESULT_FILE));
+    }
+
+    @Test
+    public void inputFileEmpty() throws IOException {
+        Files.writeString(Path.of(SOURCE_DATA_FILE), "");
+        calculateSkippedNumbers();
+        String actualResult = Files.readString(Path.of(RESULT_FILE));
+        assertEquals("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20", actualResult);
+    }
+
+    @Test
+    public void case1() throws IOException {
+        Files.writeString(Path.of(SOURCE_DATA_FILE), "1 3 4 6 9 10 12 13 14 16 19 20");
+        calculateSkippedNumbers();
+        String actualResult = Files.readString(Path.of(RESULT_FILE));
+        assertEquals("2\n5\n7\n8\n11\n15\n17\n18", actualResult);
+    }
+
+    @Test
+    public void case2() throws IOException {
+        Files.writeString(Path.of(SOURCE_DATA_FILE), "1 3 4 12 13 14 16 19 20");
+        calculateSkippedNumbers();
+        String actualResult = Files.readString(Path.of(RESULT_FILE));
+        assertEquals("2\n5\n6\n7\n8\n9\n10\n11\n15\n17\n18", actualResult);
+    }
+
+    @Test
+    public void case3() throws IOException {
+        Files.writeString(Path.of(SOURCE_DATA_FILE), "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20");
+        calculateSkippedNumbers();
+        String actualResult = Files.readString(Path.of(RESULT_FILE));
+        assertEquals("", actualResult);
+    }
+
+    @Test
+    public void case4() throws IOException {
+        Files.writeString(Path.of(SOURCE_DATA_FILE), "1 3 4 6 13 14");
+        calculateSkippedNumbers();
+        String actualResult = Files.readString(Path.of(RESULT_FILE));
+        assertEquals("2\n5\n7\n8\n9\n10\n11\n12\n15\n16\n17\n18\n19\n20", actualResult);
+    }
+
+    @Test
+    public void case5() throws IOException {
+        Files.writeString(Path.of(SOURCE_DATA_FILE), "1");
+        calculateSkippedNumbers();
+        String actualResult = Files.readString(Path.of(RESULT_FILE));
+        assertEquals("2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20", actualResult);
+    }
+
+    @Test
+    public void case6() throws IOException {
+        Files.writeString(Path.of(SOURCE_DATA_FILE), "20");
+        calculateSkippedNumbers();
+        String actualResult = Files.readString(Path.of(RESULT_FILE));
+        assertEquals("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19", actualResult);
+    }
+
+    @Test
+    public void case7() throws IOException {
+        Files.writeString(Path.of(SOURCE_DATA_FILE), "16 20");
+        calculateSkippedNumbers();
+        String actualResult = Files.readString(Path.of(RESULT_FILE));
+        assertEquals("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n17\n18\n19", actualResult);
+    }
+
+    @Test
+    public void case8() throws IOException {
+        Files.writeString(Path.of(SOURCE_DATA_FILE), "1 3 4 12\n13 14 16 19\n20");
+        calculateSkippedNumbers();
+        String actualResult = Files.readString(Path.of(RESULT_FILE));
+        assertEquals("2\n5\n6\n7\n8\n9\n10\n11\n15\n17\n18", actualResult);
+    }
+
+    @Test
+    public void case9() throws IOException {
+        Files.writeString(Path.of(SOURCE_DATA_FILE), "1 2 3 4\n5 6 7 8 9\n10 11 12 13 14\n15\n16 17 18\n19 20");
+        calculateSkippedNumbers();
+        String actualResult = Files.readString(Path.of(RESULT_FILE));
+        assertEquals("", actualResult);
+    }
+
+    @Test
+    public void case10() throws IOException {
+        Files.writeString(Path.of(SOURCE_DATA_FILE), "1\n3\n4\n6\n13\n14");
+        calculateSkippedNumbers();
+        String actualResult = Files.readString(Path.of(RESULT_FILE));
+        assertEquals("2\n5\n7\n8\n9\n10\n11\n12\n15\n16\n17\n18\n19\n20", actualResult);
+    }
+
+    @Test
+    public void case11() throws IOException {
+        Files.writeString(Path.of(SOURCE_DATA_FILE), "1,. 2 3 4\n5 6- 7 8 9\n10 11 12 13. 14\n15\n16 ,17, 18\n19 20,");
+        calculateSkippedNumbers();
+        String actualResult = Files.readString(Path.of(RESULT_FILE));
+        assertEquals("", actualResult);
+    }
+
+    @Test
+    public void case12() throws IOException {
+        Files.writeString(Path.of(SOURCE_DATA_FILE), "1.-\n3\n4\n6\n;13\n14");
+        calculateSkippedNumbers();
+        String actualResult = Files.readString(Path.of(RESULT_FILE));
+        assertEquals("2\n5\n7\n8\n9\n10\n11\n12\n15\n16\n17\n18\n19\n20", actualResult);
+    }
+
+    @Test
+    public void case13() throws IOException {
+        Files.writeString(Path.of(SOURCE_DATA_FILE), "1,.2 3 4\n5 6-7 8 9\n10 11 12 13. 14\n15\n16 ,17, 18\n19 20,");
+        calculateSkippedNumbers();
+        String actualResult = Files.readString(Path.of(RESULT_FILE));
+        assertEquals("", actualResult);
+    }
+
+    private void calculateSkippedNumbers() {
+        List<Integer> numbersFromFile = writeNumbersToArray(SOURCE_DATA_FILE);
+        List<Integer> skippedNumbers = findTheMissingNumbers(numbersFromFile);
+        writeArrayToFile(skippedNumbers, RESULT_FILE);
+    }
+
+    private List<Integer> writeNumbersToArray(String sourceDataFile) {
+        List<Integer> array = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(sourceDataFile))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                for (String badSymbol : BAD_SYMBOLS) {
+                    line = line.replaceAll(badSymbol, WORD_DELIMITER);
+                }
+                String[] words = line.split(WORD_DELIMITER);
+                for (String word : words) {
+                    if (Objects.equals(word, "")) {
+                        continue;
+                    } else if (Objects.equals(word, WORD_DELIMITER)) {
+                        continue;
+                    }
+                    array.add(Integer.parseInt(word));
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Array not cleared", e);
+        }
+        return array;
+    }
+
+    private List<Integer> findTheMissingNumbers(List<Integer> numbersFromFile) {
+        List<Integer> skippedNumbers = new ArrayList<>();
+        if (numbersFromFile.size() == 0) {
+            for (int i = 1; i <= 20; i++) {
+                skippedNumbers.add(i);
+            }
+            return skippedNumbers;
+        }
+        int index = 1;
+        for (Integer numberFromFile : numbersFromFile) {
+            if (index < numberFromFile) {
+                while (index < numberFromFile) {
+                    skippedNumbers.add(index);
+                    index++;
+                }
+            } else if (index == numberFromFile) {
+                index++;
+            }
+        }
+        return skippedNumbers;
+    }
+
+    private void writeArrayToFile(List<Integer> skippedNumbers, String resultFile) {
+        try (PrintWriter pw = new PrintWriter(resultFile)) {
+            for (int i = 0; i < skippedNumbers.size(); i++) {
+                if (i == skippedNumbers.size() - 1) {
+                    pw.print(skippedNumbers.get(i));
+                    return;
+                }
+                pw.print(skippedNumbers.get(i) + "\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Array write failed", e);
+        }
+    }
+
+}
