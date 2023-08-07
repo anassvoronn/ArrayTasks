ad1c392ae44a18d7caef46720d4d5587c219bc14
anassvoronn <UQzbwQCt7R3uMfz>
2023-08-07 17:06:40 +0300

    Task32

--- src/test/java/org/dz/solution/task33/Task33Test.java
+++ src/test/java/org/dz/solution/task33/Task33Test.java
@@ -3,7 +3,11 @@ package org.dz.solution.task33;
 import org.junit.Test;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class Task33Test {
@@ -13,16 +17,28 @@ public class Task33Test {
     public static final String WORD_DELIMITER = " ";
 
     @Test
-    public void test() {
+    public void case1() throws IOException {
+        Files.writeString(Path.of(SOURCE_DATA_FILE), "кот, собака. кот( кот жираф ;котик кот");
+        writeOutTheWordsAndTheNumberOfTheirRepetitions();
+        String actualResult = Files.readString(Path.of(RESULT_FILE));
+        assertEquals("кот 4\n", actualResult);
     }
 
-    private void calculateSkippedNumbers() {
-        List<String> textFromFile = ReadingAFile(SOURCE_DATA_FILE);
-        wordCount(textFromFile);
+    @Test
+    public void case2() throws IOException {
+        Files.writeString(Path.of(SOURCE_DATA_FILE), "вода „водоем водяной « водохранилище");
+        writeOutTheWordsAndTheNumberOfTheirRepetitions();
+        String actualResult = Files.readString(Path.of(RESULT_FILE));
+        assertEquals("водяной 1\n", actualResult);
+    }
+
+    private void writeOutTheWordsAndTheNumberOfTheirRepetitions() {
+        List<String> wordList = readTextFromFile(SOURCE_DATA_FILE);
+        repCounting(wordList);
         writeWordCountToFile(RESULT_FILE);
     }
 
-    private List<String> ReadingAFile(String sourceDataFile) {
+    private List<String> readTextFromFile(String sourceDataFile) {
         List<String> array = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(sourceDataFile))) {
             String line;
@@ -40,25 +56,25 @@ public class Task33Test {
         return array;
     }
 
-    private void wordCount(List<String> array) {
-        Map<String, Integer> wordCount = new HashMap<>();
+    private void repCounting(List<String> array) {
+        Map<String, Integer> arrayOfWords = new HashMap<>();
         for (String word : array) {
-            if (wordCount.containsKey(word)) {
-                int count = wordCount.get(word);
-                wordCount.put(word, count + 1);
+            if (arrayOfWords.containsKey(word)) {
+                int count = arrayOfWords.get(word);
+                arrayOfWords.put(word, count + 1);
             } else {
-                wordCount.put(word, 1);
+                arrayOfWords.put(word, 1);
             }
         }
     }
 
     public static void writeWordCountToFile(String resultFile) {
         Map<String, Integer> wordCount = new HashMap<>();
-        try (PrintWriter writer = new PrintWriter(new FileWriter(resultFile))){
+        try (PrintWriter writer = new PrintWriter(new FileWriter(resultFile))) {
             for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
                 writer.println(entry.getKey() + " " + entry.getValue());
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Data not written to file");
             e.printStackTrace();
         }
