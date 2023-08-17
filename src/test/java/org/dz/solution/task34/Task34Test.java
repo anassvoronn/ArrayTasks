46c52ca432b5ecb298e0810c798466db9cd32dab
anassvoronn <UQzbwQCt7R3uMfz>
2023-08-17 21:32:25 +0300

    Task34

--- src/test/java/org/dz/solution/task34/Task34Test.java
+++ src/test/java/org/dz/solution/task34/Task34Test.java
@@ -7,22 +7,63 @@ public class Task34Test {
 
     @Test
     public void test1() {
-        int[] array = new int[]{2, 7, 11, 15};
-        int target = 9;
-        int[] result = {0, 1};
-        Assert.assertEquals(result, twoSum(array, target));
+        Task34Test task34 = new Task34Test();
+        int[] result = task34.twoSum(new int[]{2, 7, 11, 15}, 9);
+        Assert.assertArrayEquals(new int[]{0, 1}, result);
+    }
+
+    @Test
+    public void test2() {
+        Task34Test task34 = new Task34Test();
+        int[] result = task34.twoSum(new int[]{3, 2, 4}, 6);
+        Assert.assertArrayEquals(new int[]{1, 2}, result);
+    }
+
+    @Test
+    public void test3() {
+        Task34Test task34 = new Task34Test();
+        int[] result = task34.twoSum(new int[]{3, 3}, 6);
+        Assert.assertArrayEquals(new int[]{0, 1}, result);
+    }
+
+    @Test
+    public void test4() {
+        Task34Test task34 = new Task34Test();
+        int[] result = task34.twoSum(new int[]{13, 45, 1, 60, 88, 4}, 89);
+        Assert.assertArrayEquals(new int[]{2, 4}, result);
+    }
+
+    @Test
+    public void test5() {
+        Task34Test task34 = new Task34Test();
+        int[] result = task34.twoSum(new int[]{45, 2, -13, 0, 97, 1}, 84);
+        Assert.assertArrayEquals(new int[]{2, 4}, result);
+    }
+
+    @Test
+    public void test6() {
+        Task34Test task34 = new Task34Test();
+        int[] result = task34.twoSum(new int[]{-4, 0, 2, 55, 7}, -4);
+        Assert.assertArrayEquals(new int[]{0, 1}, result);
+    }
+
+    @Test
+    public void test7() {
+        Task34Test task34 = new Task34Test();
+        int[] result = task34.twoSum(new int[]{0, 0, 0}, 0);
+        Assert.assertArrayEquals(new int[]{0, 1}, result);
     }
 
     public int[] twoSum(int[] array, int target) {
-        int min = 0;
-        int max = array.length - 1;
-        while (min < max){
-            int currentSum = array[min] + array[max];
-            if(currentSum == target){
-                return new int[]{min, max};
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                int currentSum = array[i] + array[j];
+                if (currentSum == target) {
+                    return new int[]{i, j};
+                }
             }
-            min++;
         }
-        return new int[]{};
+
+        return new int[]{-1, -1};
     }
 }
